4bd55050ca19de505b51ae1be94e1777044c1f3b
NazonoKansatugata <0gy27b297263j6s@au.com>
2025-05-23 02:14:53 +0900

    ボタン

--- app/controller/page.tsx
+++ app/controller/page.tsx
@@ -25,25 +25,113 @@ export default function ControllerPage() {
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: 40 }}>
-      <h1>テトリス コントローラー</h1>
-      <div style={{ margin: "24px 0" }}>
-        <button style={{ fontSize: 24, margin: 8 }} onClick={() => sendInput("left")}>
-          ← 左
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        background: "#222",
+      }}
+    >
+      {/* 十字キー */}
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginRight: 60 }}>
+        <button
+          style={{
+            width: 60,
+            height: 60,
+            marginBottom: 8,
+            fontSize: 28,
+            borderRadius: 12,
+            background: "#444",
+            color: "#fff",
+            border: "none",
+          }}
+          onClick={() => sendInput("up")}
+        >
+          ↑
         </button>
-        <button style={{ fontSize: 24, margin: 8 }} onClick={() => sendInput("right")}>
-          右 →
-        </button>
-        <button style={{ fontSize: 24, margin: 8 }} onClick={() => sendInput("rotate")}>
-          ⟳ 回転
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <button
+            style={{
+              width: 60,
+              height: 60,
+              marginRight: 8,
+              fontSize: 28,
+              borderRadius: 12,
+              background: "#444",
+              color: "#fff",
+              border: "none",
+            }}
+            onClick={() => sendInput("left")}
+          >
+            ←
+          </button>
+          <button
+            style={{
+              width: 60,
+              height: 60,
+              marginLeft: 8,
+              fontSize: 28,
+              borderRadius: 12,
+              background: "#444",
+              color: "#fff",
+              border: "none",
+            }}
+            onClick={() => sendInput("right")}
+          >
+            →
+          </button>
+        </div>
+        <button
+          style={{
+            width: 60,
+            height: 60,
+            marginTop: 8,
+            fontSize: 28,
+            borderRadius: 12,
+            background: "#444",
+            color: "#fff",
+            border: "none",
+          }}
+          onClick={() => sendInput("down")}
+        >
+          ↓
         </button>
       </div>
-      <div style={{ margin: "24px 0" }}>
-        <button style={{ fontSize: 24, margin: 8 }} onClick={() => sendInput("soft_drop")}>
-          ↓ ソフトドロップ
+      {/* A/Bボタン */}
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginLeft: 60 }}>
+        <button
+          style={{
+            width: 80,
+            height: 80,
+            marginBottom: 24,
+            fontSize: 32,
+            borderRadius: "50%",
+            background: "#e74c3c",
+            color: "#fff",
+            border: "none",
+            boxShadow: "0 2px 8px #0006",
+          }}
+          onClick={() => sendInput("A")}
+        >
+          A
         </button>
-        <button style={{ fontSize: 24, margin: 8 }} onClick={() => sendInput("hard_drop")}>
-          ⏬ ハードドロップ
+        <button
+          style={{
+            width: 80,
+            height: 80,
+            fontSize: 32,
+            borderRadius: "50%",
+            background: "#3498db",
+            color: "#fff",
+            border: "none",
+            boxShadow: "0 2px 8px #0006",
+          }}
+          onClick={() => sendInput("B")}
+        >
+          B
         </button>
       </div>
     </div>
